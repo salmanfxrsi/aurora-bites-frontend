2cd21132f45eb9530c303f568b7f8610929b1790
salmanfxrsi <salmanxprivate@gmail.com>
2025-01-08 19:08:34 +0600

    feat: add custom hooks and enhance FoodCardWithButton functionality
    - Created  hook to provide access to the AuthContext using useContext.
    - Implemented  hook to encapsulate an Axios instance with a base URL.
    - Enhanced the  component to include:
      -  function for adding items to the cart using .
      - Integrated user authentication check to ensure users are logged in before adding to the cart.
      - Redirect to login page if the user is not authenticated.
      - Toast notifications for successful addition to the cart and error handling.
    
    These changes streamline context usage, secure API calls, and improve user experience for cart operations.

--- src/components/FoodCardWithButton.jsx
+++ src/components/FoodCardWithButton.jsx
@@ -1,6 +1,39 @@
+import toast from "react-hot-toast";
+import useAuth from "../hooks/useAuth";
+import { useLocation, useNavigate } from "react-router-dom";
+import useAxiosSecure from "../hooks/useAxiosSecure";
+
 /* eslint-disable react/prop-types */
 const FoodCardWithButton = ({ food }) => {
-  const { name, recipe, image, price } = food;
+  const { _id, name, recipe, image, price } = food;
+  const { user } = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const axiosSecure = useAxiosSecure();
+
+  const handleAddToCart = () => {
+    if (user && user.email) {
+      const cartItem = {
+        menuId: _id,
+        email: user.email,
+        name,
+        recipe,
+        image,
+        price,
+      };
+      try{
+        axiosSecure.post('/carts', cartItem)
+        toast.success(`${name} Added To Cart`)
+      }catch(error){
+        toast.error(error.message)
+      }
+    } else {
+      toast.error("SignIn To Add To Cart");
+      navigate('/login', { state: { from: location } })
+      return;
+    }
+  };
+
   return (
     <div className="card bg-base-100 shadow-xl">
       <figure>
@@ -15,7 +48,10 @@ const FoodCardWithButton = ({ food }) => {
         </h2>
         <p className="text-[#151515] mb-6">{recipe}</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-warning mx-auto btn-outline">
+          <button
+            onClick={handleAddToCart}
+            className="btn btn-warning mx-auto btn-outline"
+          >
             ADD TO CART
           </button>
         </div>
